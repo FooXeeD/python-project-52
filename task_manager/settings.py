f1ad09654884869299daa00da4a4fcbe3e112324
fooxeed <fooxeed@mail.ru>
2024-08-02 23:47:41 +1000

    Final 2

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -18,15 +18,20 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'True'
 
+
+
 ALLOWED_HOSTS = [
     'webserver',
     '127.0.0.1',
     '0.0.0.0',
     '.railway.app',
     'localhost',
-    'python-project-52-lay3.onrender.com'
+    'onrender'
 ]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 # Application definition
 
 INSTALLED_APPS = [
